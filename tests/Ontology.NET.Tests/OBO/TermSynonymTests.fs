b5aff6f44763b6c0650a4a842bffc96adaa4838a
omaus <omaus@bio.uni-kl.de>
2024-11-12 11:03:19 +0100

    Add tests for OBO functionality

---
+++ tests/Ontology.NET.Tests/OBO/TermSynonymTests.fs
@@ -0,0 +1,23 @@
+﻿namespace Ontology.NET.Tests.OBO
+
+
+open Expecto
+open Ontology.NET.OBO
+
+
+module TermSynonymTests =
+
+    [<Tests>]
+    let main =
+        testList "TermSynonym" [
+            testCase "create" <| fun _ ->
+                let actual = TermSynonym.create("\"Bacillus coli\"", Related, "synonym", [])
+                Expect.equal actual.Text "\"Bacillus coli\"" "text"
+                Expect.equal actual.Scope Related "scope"
+                Expect.equal actual.TypeName "synonym" "typeName"
+                Expect.equal actual.DBXrefs [] "dbxrefs"
+            testCase "ToOboString" <| fun _ ->
+                let actual = TermSynonym.create("\"Bacillus coli\"", Related, "synonym", []).ToLine()
+                let expected = "\"Bacillus coli\" RELATED synonym []"
+                Expect.equal actual expected "is not equal"
+        ]
