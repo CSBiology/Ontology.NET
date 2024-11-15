f7e1251db901bc9ee90e07f09b607b83418f8d07
omaus <omaus@bio.uni-kl.de>
2024-11-15 09:22:29 +0100

    Add types and functions for ontology relations

---
+++ src/Ontology.NET/Relation.fs
@@ -0,0 +1,76 @@
+﻿namespace Ontology.NET
+
+
+open ControlledVocabulary
+open Ontology.NET.OBO
+
+
+type Relationship =
+    | Synonym
+    | Xref
+    | Is_a
+    | Part_of
+    | Custom of string
+
+    with
+        ///
+        static member createFromString str =
+            match str with
+            | "is_a"    -> Is_a
+            | "part_of" -> Part_of
+            | "synonym" -> Synonym
+            | "xref"    -> Xref
+            | _         -> Custom str
+
+        ///
+        static member fromOboTerm (oboTerm : OboTerm) =
+
+            let sourceTerm = OboTerm.toCvTerm oboTerm
+
+            let oboRelationships = 
+                oboTerm.Relationships
+                |> Seq.map (
+                    fun r ->
+                        let rString, targetTermString = OboTerm.deconstructRelationship r
+                        let targetTerm = CvTerm.create(name = "<missing>", accession = targetTermString, ref = "<missing>")
+                        let relationship = Relationship.createFromString rString
+                        sourceTerm, relationship, targetTerm
+                )
+
+            let synonyms =
+                oboTerm.Synonyms
+                |> Seq.collect (
+                    fun s ->
+                        let targetTerms = 
+                            s.DBXrefs 
+                            |> Seq.map DBXref.toCvTerm
+                        targetTerms
+                        |> Seq.map (fun tt -> sourceTerm, Synonym, tt)
+                )
+
+            let isAs =
+                oboTerm.IsA
+                |> Seq.map (
+                    fun i ->
+                        let targetTerm = CvTerm.create(name = "<missing>", accession= i, ref = "<missing>")
+                        sourceTerm, Is_a, targetTerm
+                )
+
+            let xRefs =
+                let targetTerms =
+                    oboTerm.Xrefs
+                    |> Seq.map DBXref.toCvTerm
+                targetTerms
+                |> Seq.map (fun tt -> sourceTerm, Xref, tt)
+
+            Seq.concat [oboRelationships; synonyms; isAs; xRefs]
+
+
+type Relation =
+    | Single of Relationship
+    | Composite of Relationship
+
+    with
+        static member fromOboTerm (oboTerm: OboTerm) =
+            Relationship.fromOboTerm oboTerm
+            |> Seq.map (fun (st,r,tt) -> st, Single r, tt)
---
+++ src/Ontology.NET/RelationExtensions.fs
@@ -0,0 +1,16 @@
+﻿namespace Ontology.NET
+
+
+open ControlledVocabulary
+open Ontology.NET.OBO
+
+
+module OboTerm =
+
+    /// Creates ontology relations from a given OboTerm.
+    let toRelations (oboTerm : OboTerm) =
+        Relationship.fromOboTerm oboTerm
+
+    /// Creates ontology relationships from a given OboTerm.
+    let toRelationships (oboTerm : OboTerm) =
+        Relation.fromOboTerm oboTerm
