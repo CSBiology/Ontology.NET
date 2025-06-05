f7c087a391c17b4d653b35628a36023aafe399df
Oliver Maus <oliver.maus@eclipso.de>
2025-06-05 18:10:32 +0200

    Add OntologyAnnotation functionality to OboTerm extensions

---
+++ src/Ontology.NET.Extensions/OBO/OboTermExtensions.fs
@@ -0,0 +1,15 @@
+﻿namespace Ontology.NET.OBO
+
+
+open ARCtrl
+
+
+module OboTerm =
+
+    /// Translates an OBO term into an ISADotNet OntologyAnnotation.
+    let toOntologyAnnotation (term : OboTerm) =
+        OntologyAnnotation(term.Name,tan = term.Id)
+
+    /// Translates an ISADotNet OntologyAnnotation into an OBO term.
+    let ofOntologyAnnotation (term : OntologyAnnotation) =
+        OboTerm.Create(term.TermAccessionShort,term.NameText)
