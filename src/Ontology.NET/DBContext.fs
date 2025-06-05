fbe3ef88962738e6867a729d8f6977239872a6ca
Oliver Maus <oliver.maus@eclipso.de>
2025-06-05 18:08:49 +0200

    Add GenericOntology and DBContext types

---
+++ src/Ontology.NET/DBContext.fs
@@ -0,0 +1,15 @@
+﻿namespace Ontology.NET
+
+
+open ControlledVocabulary
+
+open System.Collections.Generic
+
+
+type DBContext() =
+
+    inherit Dictionary<CvTerm,CvTerm Set>()
+
+    /// 
+    static member fromOntology onto =
+        
---
+++ src/Ontology.NET/GenericOntology.fs
@@ -0,0 +1,15 @@
+﻿namespace Ontology.NET
+
+
+open ControlledVocabulary
+
+open System.Collections.Generic
+
+
+type GenericOntology() =
+
+    inherit Dictionary<CvTerm,CvTerm Set>()
+
+    ///
+    static member fromOboOntology obo =
+        
