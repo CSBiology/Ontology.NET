74b71c29413c479a4a051fbfcd84321bbfea936b
Oliver Maus <oliver.maus@eclipso.de>
2025-06-06 18:21:36 +0200

    (WIP) Update GenericOntology

--- src/Ontology.NET/GenericOntology.fs
+++ src/Ontology.NET/GenericOntology.fs
@@ -2,14 +2,28 @@
 
 
 open ControlledVocabulary
+open Ontology.NET.OBO
 
 open System.Collections.Generic
 
 
 type GenericOntology() =
 
-    inherit Dictionary<CvTerm,CvTerm Set>()
+    inherit Dictionary<CvTerm,(string*CvTerm) Set>()
 
     ///
-    static member fromOboOntology obo =
-        
+    static member fromOboOntology (obo : OboOntology) =
+        let newGO = GenericOntology()
+        obo.Terms
+        |> List.iter (
+            fun ot ->
+                let rels =
+                    ot.
+        )
+        let cvts, rels =
+            obo.Terms
+            |> List.map (
+                fun ot -> CvTerm.create(ot.Id, ot.Name, CvTerm.refOfAccession ot.Id)
+            )
+        let rels =
+            obo.Terms
