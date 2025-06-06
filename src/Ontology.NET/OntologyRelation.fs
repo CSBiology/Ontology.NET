43ac300f75ca0dd46f4ce9aace973422cf71f8f0
Oliver Maus <oliver.maus@eclipso.de>
2025-06-06 17:41:28 +0200

    Add generic types for ontological relations

---
+++ src/Ontology.NET/OntologyRelation.fs
@@ -0,0 +1,23 @@
+﻿namespace Ontology.NET
+
+
+open ControlledVocabulary
+
+
+/// Model for a generic type of relation in an ontology. The relation can be of type CvTerm when defined (recommended) or string when not defined.
+type RelationType =
+    | Term of CvTerm
+    | Custom of string
+
+
+/// Model for a generic ontological relation. Consists of fields RelationType that inhabits the type of the relation, and Target for the targeted CvTerm of the relation.
+type OntologyRelation = {
+    RelationType    : RelationType
+    Target          : CvTerm
+} with
+
+    /// Creates an OntologyRelation with the given RelationType and target CvTerm.
+    static member create relationType targetTerm = {
+        RelationType    = relationType
+        Target          = targetTerm
+    }
