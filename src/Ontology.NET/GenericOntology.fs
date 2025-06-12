4a467f4c9464aee0dd81ebe387615945a5fa3438
Oliver Maus <oliver.maus@eclipso.de>
2025-06-12 18:35:18 +0200

    (WIP) Work on ontology transform

--- src/Ontology.NET/GenericOntology.fs
+++ src/Ontology.NET/GenericOntology.fs
@@ -18,12 +18,34 @@ type GenericOntology() =
         |> List.iter (
             fun ot ->
                 let rels =
-                    ot.Xrefs
+                    let xrefCvTs =
+                        ot.Xrefs
+                        |> List.map
         )
         let cvts, rels =
             obo.Terms
-            |> List.map (
-                fun ot -> CvTerm.create(ot.Id, ot.Name, CvTerm.refOfAccession ot.Id)
+            |> List.iter (
+                fun ot -> 
+                    let term = CvTerm.create(ot.Id, ot.Name, CvTerm.refOfAccession ot.Id)
+                    let rels =
+                        let xrefCvTs =
+                            ot.Xrefs
+                            |> List.map (
+                                fun xref -> CvTerm.create(xref.Name, "<missing>", CvTerm.refOfAccession xref.Name)
+                            )
+                        let isACvTs =
+                            ot.IsA
+                            |> List.map (
+                                fun isA -> 
+                                    obo.Terms
+                                    |> List.tryFind (
+                                        fun ot2 ->
+                                            ot2.Id = isA
+                                    )
+                                    |> Option.defaultValue (CvTerm.create())
+                            )
+                        
+                    newGO.Add(term, rels)
             )
         let rels =
             obo.Terms
