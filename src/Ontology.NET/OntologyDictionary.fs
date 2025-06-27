c2fe9d8fb594c0e1fcdc72d066a7bae8e9cdd624
Oliver Maus <oliver.maus@eclipso.de>
2025-06-27 19:33:37 +0200

    (WIP) Update generic Ontology class

---
+++ src/Ontology.NET/OntologyDictionary.fs
@@ -0,0 +1,53 @@
+﻿namespace Ontology.NET
+
+
+open ControlledVocabulary
+open Ontology.NET.OBO
+
+open System.Collections.Generic
+
+
+type OntologyDictionary() =
+
+    inherit Dictionary<CvTerm,OntologyRelation Set>()
+
+    ///
+    static member fromOboOntology (oboOnto : OboOntology) =
+
+        let onto = Ontology()
+
+        let cvts = Seq.map OboTerm.toCvTerm oboOnto.Terms
+
+        let rels =
+            oboOnto.Terms
+            |> List.iter (
+                fun oboTerm ->
+                    let cvt = OboTerm.toCvTerm oboTerm
+                    let isAs =
+                        oboTerm.IsA
+                        |> Seq.map (
+                            fun isATermId ->
+                                cvts
+                                |> Seq.tryFind (
+                                    fun searchedTerm ->
+                                        searchedTerm.Accession = isATermId
+                                )
+                                |> Option.defaultValue (CvTerm.create(accession = isATermId, name = "<missing>", ref = "<missing>"))
+                        )
+                        |> Seq.map (OntologyRelation.create IsA)
+                    let xrefs =
+                        oboTerm.Xrefs
+                        |> Seq.map (
+                            fun xref ->
+                                cvts
+                                |> Seq.tryFind (
+                                    fun searchedTerm ->
+                                        searchedTerm.Accession = xref
+                                )
+                                xref.Name
+                        )
+
+
+            )
+
+        onto
