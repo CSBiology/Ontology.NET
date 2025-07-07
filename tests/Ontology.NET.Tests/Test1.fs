99de0347104ffe02a1aa1fab3c7a6938493ebcaa
Oliver Maus <oliver.maus@eclipso.de>
2025-07-07 17:00:50 +0200

    Add test project to solution

---
+++ tests/Ontology.NET.Tests/Test1.fs
@@ -0,0 +1,11 @@
+﻿namespace Ontology.NET.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type Test1 () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
