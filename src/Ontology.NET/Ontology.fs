3fb00c1358ae583223b92884efba74fdbde9e1ce
Oliver Maus <oliver.maus@eclipso.de>
2025-07-08 19:26:36 +0200

    (WIP) Add additional term related functionality

--- src/Ontology.NET/Ontology.fs
+++ src/Ontology.NET/Ontology.fs
@@ -82,6 +82,9 @@ type Ontology() =
 
         onto
 
+
+    // Xref functionality:
+
     /// Returns the term IDs of all terms that have an Xref relation to the given term.
     member this.GetXrefs termId =
         FContext.neighbours this[termId]
@@ -110,24 +113,94 @@ type Ontology() =
     static member getXrefsAsTerms termId (onto : Ontology) =
         onto.GetXrefsAsTerms termId
 
+
+    // Target relation functionality:
+
     /// <summary>
-    /// Returns all terms that are transitively related to the given term ID, following only those relations for which the provided predicate returns true. The traversal is performed depth-first.
+    /// Returns all terms that are transitively target-related to the given term ID, following only those relations for which the provided predicate returns true. The traversal is performed depth-first.
     /// </summary>
     /// <param name="termID">The ID of the starting term.</param>
     /// <param name="predicate">A function that takes the current term ID, the corresponding CvTerm, and its outgoing relations, and returns a boolean indicating whether traversal should follow that term.</param>
-    /// <returns>A sequence of term IDs representing all related terms reachable by recursively following valid relations as defined by the predicate.</returns>
-    member this.GetRelatedTermsBy(termID, predicate) =
+    /// <returns>A sequence of term IDs and corresponding CvTerms representing all target-related terms reachable by recursively following valid relations as defined by the predicate.</returns>
+    /// <remarks>A target relation is an outgoing relation. E.g. "Term A -> Term B", Term B is the target-related term to Term A.</remarks>
+    member this.GetTargetTermsBy(termID, predicate) =
         Algorithms.DFS.ofFGraphBy termID predicate this
 
-    /// <summary>Takes a term ID, a predicate function and an Ontology, and returns all related terms where predicate returned true.</summary>
-    /// <param name="predicate">Function that takes a term ID, a CvTerm and a RelationType Set and returns bool.</param>
-    static member getRelatedTermsBy termID predicate (onto : Ontology) =
-        onto.GetRelatedTermsBy(termID, predicate)
+    /// <summary>
+    /// Returns all terms that are transitively target-related to the given term ID, following only those relations for which the provided predicate returns true. The traversal is performed depth-first.
+    /// </summary>
+    /// <param name="termID">The ID of the starting term.</param>
+    /// <param name="predicate">A function that takes the current term ID, the corresponding CvTerm, and its outgoing relations, and returns a boolean indicating whether traversal should follow that term.</param>
+    /// <param name="onto">The Ontology on which the operation is performed.</param>
+    /// <returns>A sequence of term IDs and corresponding CvTerms representing all target-related terms reachable by recursively following valid relations as defined by the predicate.</returns>
+    /// <remarks>A target relation is an outgoing relation. E.g. "Term A -> Term B", Term B is the target-related term to Term A.</remarks>
+    static member getTargetTermsBy termID predicate (onto : Ontology) =
+        onto.GetTargetTermsBy(termID, predicate)
 
     /// <summary>
-    /// Returns all terms that are transitively related to the given term ID, but limits the traversal to the specified depth. The traversal is performed depth-first.</summary>
+    /// Returns all terms that are transitively target-related to the given term ID, but limits the traversal to the specified depth. The traversal is performed depth-first.</summary>
     /// <param name="termID">The ID of the starting term.</param>
     /// <param name="depth">The maximum depth to traverse. A depth of 0 returns only the starting term.</param>
-    /// <returns>A sequence of term IDs representing all related terms that can be reached within the given depth, where depth corresponds to the number of relation steps (edges) from the starting term.</returns>
-    member this.GetRelatedTermsWithDepth(termID, depth) =
+    /// <returns>A sequence of term IDs and corresponding CvTerms representing all target-related terms that can be reached within the given depth, where depth corresponds to the number of relation steps (edges) from the starting term.</returns>
+    /// <remarks>A target relation is an outgoing relation. E.g. "Term A -> Term B", Term B is the target-related term to Term A.</remarks>
+    member this.GetTargetTermsWithDepth(termID, depth) =
         Algorithms.DFS.ofFGraphWithDepth termID depth this
+
+    /// <summary>
+    /// Returns all terms that are transitively target-related to the given term ID, but limits the traversal to the specified depth. The traversal is performed depth-first.</summary>
+    /// <param name="termID">The ID of the starting term.</param>
+    /// <param name="depth">The maximum depth to traverse. A depth of 0 returns only the starting term.</param>
+    /// <param name="onto">The Ontology on which the operation is performed.</param>
+    /// <returns>A sequence of term IDs and corresponding CvTerms representing all target-related terms that can be reached within the given depth, where depth corresponds to the number of relation steps (edges) from the starting term.</returns>
+    /// <remarks>A target relation is an outgoing relation. E.g. "Term A -> Term B", Term B is the target-related term to Term A.</remarks>
+    static member getTargetTermsWithDepth termID depth (onto : Ontology) =
+        onto.GetTargetTermsWithDepth(termID, depth)
+
+
+    // Source relation functionality:
+
+    member this.GetSourceTermsBy(termID, predicate) =
+        
+
+
+    // SuperClass functionality:
+
+    /// <summary>
+    /// Returns all terms that are transitively target-related via an is_a relation to the given term ID.
+    /// </summary>
+    /// <param name="termID">The ID of the starting term.</param>
+    /// <returns>A sequence of term IDs representing all is_a target-related terms.</returns>
+    member this.GetSuperClassesTransitively(termID) =
+        this.GetTargetTermsBy(termID, fun _ _ e -> Set.contains IsA e) |> Seq.map fst
+
+    /// <summary>
+    /// Returns all terms that are transitively target-related via an is_a relation to the given term ID.
+    /// </summary>
+    /// <param name="termID">The ID of the starting term.</param>
+    /// <param name="onto">The Ontology on which the operation is performed.</param>
+    /// <returns>A sequence of term IDs representing all is_a target-related terms.</returns>
+    static member getSuperClassesTransitively termID (onto : Ontology) =
+        onto.GetSuperClassesTransitively(termID)
+
+    /// <summary>
+    /// Returns all terms that are transitively target-related via an is_a relation to the given term ID.
+    /// </summary>
+    /// <param name="termID">The ID of the starting term.</param>
+    /// <returns>A sequence of CvTerms representing all is_a target-related terms.</returns>
+    member this.GetSuperClassesTransitivelyAsTerms(termID) =
+        this.GetTargetTermsBy(termID, fun _ _ e -> Set.contains IsA e) |> Seq.map snd
+
+    /// <summary>
+    /// Returns all terms that are transitively target-related via an is_a relation to the given term ID.
+    /// </summary>
+    /// <param name="termID">The ID of the starting term.</param>
+    /// <param name="onto">The Ontology on which the operation is performed.</param>
+    /// <returns>A sequence of CvTerms representing all is_a target-related terms.</returns>
+    static member getSuperClassesTransitivelyAsTerms termID (onto : Ontology) =
+        onto.GetSuperClassesTransitivelyAsTerms(termID)
+
+
+    // SubClass functionality:
+
+    member this.GetSubClassesTransitively(termID) =
+        FContext.predecessors this[termID]
