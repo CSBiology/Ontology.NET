39b00f589bde084cdc33506e4051a9493c5ce00e
omaus <oliver.maus@eclipso.de>
2025-07-14 19:07:01 +0200

    Add unit test for AddTerm

--- tests/Ontology.NET.Tests/Ontology.Tests.fs
+++ tests/Ontology.NET.Tests/Ontology.Tests.fs
@@ -2,7 +2,9 @@
 
 
 open Expecto
+open Graphoscope
 
+open ControlledVocabulary
 open Ontology.NET
 
 
@@ -27,6 +29,18 @@ module OntologyTests =
                     let actual = ReferenceObjects.testOnto1.GetXrefs "test:03" |> Seq.toList
                     let expected = ["test:02"; "test:01"]
                     Expect.sequenceEqual actual expected "Xref list is not correct"
+
+                testCase "returns correct Xrefs on term ID test:04" <| fun _ ->
+                    let actual = ReferenceObjects.testOnto1.GetXrefs "test:04" |> Seq.toList
+                    Expect.isTrue (List.isEmpty actual) "Xref list is not empty"
+            ]
+
+            testList "AddTerm" [
+                testCase "adds terms correctly" <| fun _ ->
+                    let testOnto = Ontology()
+                    let testTerm = CvTerm.create("test:0", "testTerm", "test")
+                    testOnto.AddTerm testTerm |> ignore
+                    Expect.isTrue (FGraph.containsNode testTerm.Accession testOnto) "Does not contain newly added test term"
             ]
 
         ]
