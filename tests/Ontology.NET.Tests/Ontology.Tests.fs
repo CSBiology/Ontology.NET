e67da5f246b6d1c1bcd504ad54010aa5f8191f8a
omaus <oliver.maus@eclipso.de>
2025-07-14 18:14:39 +0200

    Add GetXref unit tests

---
+++ tests/Ontology.NET.Tests/Ontology.Tests.fs
@@ -0,0 +1,32 @@
+﻿namespace Ontology.NET.Tests
+
+
+open Expecto
+
+open Ontology.NET
+
+
+module OntologyTests =
+
+    [<Tests>]
+    let ontologyTest =
+        testList "Ontology" [
+
+            testList "GetXrefs" [
+                testCase "returns correct Xrefs on term ID test:01" <| fun _ ->
+                    let actual = ReferenceObjects.testOnto1.GetXrefs "test:01" |> Seq.toList
+                    let expected = ["test:02"; "test:03"]
+                    Expect.sequenceEqual actual expected "Xref list is not correct"
+
+                testCase "returns correct Xrefs on term ID test:02" <| fun _ ->
+                    let actual = ReferenceObjects.testOnto1.GetXrefs "test:02" |> Seq.toList
+                    let expected = ["test:03"; "test:01"]
+                    Expect.sequenceEqual actual expected "Xref list is not correct"
+
+                testCase "returns correct Xrefs on term ID test:03" <| fun _ ->
+                    let actual = ReferenceObjects.testOnto1.GetXrefs "test:03" |> Seq.toList
+                    let expected = ["test:02"; "test:01"]
+                    Expect.sequenceEqual actual expected "Xref list is not correct"
+            ]
+
+        ]
