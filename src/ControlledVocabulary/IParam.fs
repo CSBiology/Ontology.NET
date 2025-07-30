23dab3e4e776c0b3207b63add8df95e48946ec8e
Oliver Maus <oliver.maus@eclipso.de>
2025-07-30 15:16:59 +0200

    Move CV functionality from ARCTokenization

---
+++ src/ControlledVocabulary/IParam.fs
@@ -0,0 +1,149 @@
+﻿namespace ControlledVocabulary
+
+
+/// Interface ensures the properties necessary for Param.
+type IParam =
+
+    inherit ICvBase
+    inherit IParamBase
+
+
+/// Functions for working Param objects.
+type Param =
+
+    //---------------------- IParamBase implementations ----------------------//
+
+    /// Returns the value of the Param as a ParamValue.
+    static member getParamValue (param: IParam) = 
+        ParamBase.getParamValue param
+
+    /// Returns the value of the Param as IConvertible.
+    static member getValue (param: IParam) = 
+        ParamBase.getValue param
+
+    /// Returns the value of the Param as string.
+    static member getValueAsString (param: IParam) = 
+        ParamBase.getValueAsString param
+
+    /// Returns the value of the Param as int if possible, else fails.
+    static member getValueAsInt (param: IParam) = 
+        ParamBase.getValueAsInt param
+
+    /// Returns the value of the Param as a CvTerm.
+    static member getValueAsTerm (param: IParam) = 
+        ParamBase.getValueAsTerm param
+
+    /// Returns the accession of the Param's value if it exists. Else returns None.
+    static member tryGetValueAccession (param: IParam) = 
+        ParamBase.tryGetValueAccession param
+
+    /// Returns the reference of the Param's value if it exists. Else returns None.
+    static member tryGetValueRef (param: IParam) = 
+        ParamBase.tryGetValueRef param
+
+    /// Returns the CvUnit of the Param if it exists. Else returns None.
+    static member tryGetCvUnit (param: IParam) : CvUnit option = 
+        ParamBase.tryGetCvUnit param
+
+    /// Returns the CvUnit's value of the Param if it exists. Else returns None.
+    static member tryGetCvUnitValue (param: IParam) = 
+        ParamBase.tryGetCvUnitValue param
+
+    /// Returns the CvUnit's term name of the Param if it exists. Else returns None.
+    static member tryGetCvUnitTermName (param: IParam) = 
+        ParamBase.tryGetCvUnitTermName param
+
+    /// Returns the CvUnit's term accession of the Param if it exists. Else returns None.
+    static member tryGetCvUnitTermAccession (param: IParam) = 
+        ParamBase.tryGetCvUnitTermAccession param
+
+    /// Returns the CvUnit's term ref of the Param if it exists. Else returns None.
+    static member tryGetCvUnitTermRef (param: IParam) = 
+        ParamBase.tryGetCvUnitTermRef param
+
+    /// Maps the value of the Param by appyling the given function.
+    static member mapValue (f : ParamValue -> ParamValue) (param : IParam) = 
+        ParamBase.mapValue f param :?> IParam
+
+    /// Tries to map the value of the Param by applying the given function and returns Some if successful. Else returns None.
+    static member tryMapValue (f : ParamValue -> ParamValue option) (param : IParamBase) = 
+        ParamBase.tryMapValue f param 
+        |> Option.map (fun v -> v :?> IParam)
+
+    /// Tries to add a name to the Param and returns Some if successful. Else returns None.
+    static member tryAddName (name : string) (param : IParamBase) = 
+        ParamBase.tryAddName name param
+        |> Option.map (fun v -> v :?> IParam)
+
+    /// Tries to add an accession to the Param and returns Some if successful. Else returns None.
+    static member tryAddAccession (acc : string) (param : IParamBase) = 
+        ParamBase.tryAddAccession acc param
+        |> Option.map (fun v -> v :?> IParam)
+
+    /// Tries to add a reference to the Param and returns Some if successful. Else returns None.
+    static member tryAddReference (ref : string) (param : IParamBase) = 
+        ParamBase.tryAddReference ref param
+        |> Option.map (fun v -> v :?> IParam)
+
+    /// Tries to add a unit to the Param and returns Some if successful. Else returns None.
+    static member tryAddUnit (unit : CvUnit) (param : IParamBase) = 
+        ParamBase.tryAddUnit unit param
+        |> Option.map (fun v -> v :?> IParam)
+
+    //------------------------ ICvBase implementations -----------------------//
+    
+    /// Returns the ID of the CV item.
+    static member getCvAccession (param: IParam) = 
+        CvBase.getCvAccession param
+
+    /// Returns the name of the CV item.
+    static member getCvName (param: IParam) = 
+        CvBase.getCvName param
+
+    /// Returns the reference of the CV item.
+    static member getCvRef (param: IParam) = 
+        CvBase.getCvRef param
+
+    /// Returns the full term of the CV item.
+    static member getTerm (param: IParam) = 
+        CvBase.getTerm param
+
+    /// Returns true if the given term matches the term of the CV item.
+    static member equalsTerm (term : CvTerm) (param: IParam) = 
+        CvBase.equalsTerm term param
+
+    /// Returns true if the terms of the given Param items match.
+    static member equals (param1 : IParam) (param2 : IParam) = 
+        CvBase.equals param1 param2
+
+    /// Returns true if the names of the given Param items match.
+    static member equalsName (param1 : IParam) (param2 : IParam) = 
+        CvBase.equalsName param1 param2
+
+    /// Returns Some Value of type 'T if the given Param item can be downcast, else returns None.
+    static member inline tryAs<'T when 'T :> IParam> (param: IParam) = 
+        CvBase.tryAs<'T> param
+
+    /// Returns true if the given Param item can be downcast.
+    static member inline is<'T when 'T :> IParam> (param : IParam) = 
+        CvBase.is<'T> param
+
+    //-------------------- IParam specific implementations -------------------//
+
+
+[<AutoOpen>]
+module IParamExtensions =
+
+    type CvBase with
+        /// Returns Some Param if the given param item can be downcast, else returns None.
+        static member tryParam (cv : ICvBase) =
+            match cv with
+            | :? IParam as param -> Some param
+            | _ -> None
+
+    type ParamBase with
+        /// Returns Some Param, if the given value item can be downcast, else returns None.
+        static member tryParam (cv : IParamBase) =
+            match cv with
+            | :? IParam as param -> Some param
+            | _ -> None
---
+++ src/ControlledVocabulary/UserParam.fs
@@ -0,0 +1,215 @@
+﻿namespace ControlledVocabulary
+
+
+open System.Collections.Generic
+
+
+/// Represents a structured value, annotated by a user defined name.
+[<StructuredFormatDisplay("{DisplayText}")>]
+type UserParam(name : string, paramValue : ParamValue, attributes : IDictionary<string,IParam>) =
+
+    inherit CvAttributeCollection(attributes)        
+
+    member this.Accession   = name
+    member this.Name        = name
+    member this.RefUri      = "UserTerm"
+    member this.Value       = paramValue
+    member this.WithValue(v : ParamValue) = UserParam(name,v,attributes)
+    member this.HasAttributes 
+        with get() = this.Attributes |> Seq.isEmpty |> not
+
+    interface IParam with 
+        member this.Accession                   = this.Accession
+        member this.Name                        = this.Name     
+        member this.RefUri                      = this.RefUri   
+        member this.Value                       = this.Value    
+        member this.WithValue(v : ParamValue)   = this.WithValue(v)
+        member this.HasAttributes               = this.HasAttributes
+
+    new (name,pv,attributes : seq<IParam>) = 
+        let dict = CvAttributeCollection(attributes)
+        UserParam (name,pv,dict)
+    new (name,pv) = 
+        UserParam (name,pv,Seq.empty)
+
+    /// Serves as the default hash function.
+    override this.GetHashCode() =
+        hash (name, paramValue, attributes)
+
+    /// Determines whether the specified object is equals to the current object.
+    override this.Equals(o) =
+        match o with
+        | :? CvTerm as cvt -> Param.equalsTerm cvt this
+        | :? UserParam as up -> 
+            up.Name         = this.Name &&
+            up.Value        = this.Value &&
+            up.Attributes   = this.Attributes
+        | :? IParam as p ->
+            p.Name  = this.Name &&
+            p.Value = this.Value
+        | :? ICvBase as cvb -> CvBase.equals cvb this
+        | :? IParamBase as pb -> pb.Value = this.Value
+        | _ -> false
+
+
+    //---------------------- IParam implementations ----------------------//
+
+    /// Returns the value of the UserParam as a ParamValue.
+    static member getParamValue (up: UserParam) = 
+        Param.getParamValue up
+
+    /// Returns the value of the UserParam as IConvertible.
+    static member getValue (up: UserParam) = 
+        Param.getValue up
+
+    /// Returns the value of the UserParam as string.
+    static member getValueAsString (up: UserParam) = 
+        Param.getValueAsString up
+        
+    /// Returns the value of the UserParam as int if possible, else fails.
+    static member getValueAsInt (up: UserParam) = 
+        Param.getValueAsInt up
+
+    /// Returns the value of the UserParam as a CvTerm.
+    static member getValueAsTerm (up: UserParam) = 
+        Param.getValueAsTerm up
+
+    /// Returns the value's accession of the UserParam if it exists. Else returns None.
+    static member tryGetValueAccession (up: UserParam) = 
+        Param.tryGetValueAccession up
+
+    /// Returns the value's reference of the UserParam if it exists. Else returns None.
+    static member tryGetValueRef (up: UserParam) = 
+        Param.tryGetValueRef up
+
+    /// Returns the value's CvUnit of the UserParam if it exists. Else returns None.
+    static member tryGetCvUnit (up: UserParam) : CvUnit option = 
+        Param.tryGetCvUnit up
+
+    /// Returns the value's CvUnit's value of the UserParam if it exists. Else returns None.
+    static member tryGetCvUnitValue (up: UserParam) = 
+        Param.tryGetCvUnitValue up
+
+    /// Returns the value's CvUnit's term name of the UserParam if it exists. Else returns None.
+    static member tryGetCvUnitTermName (up: UserParam) = 
+        Param.tryGetCvUnitTermName up
+
+    /// Returns the value's CvUnit's term accession of the UserParam if it exists. Else returns None.
+    static member tryGetCvUnitTermAccession (up: UserParam) = 
+        Param.tryGetCvUnitTermAccession up
+
+    /// Returns the value's CvUnit's term reference of the UserParam if it exists. Else returns None.
+    static member tryGetCvUnitTermRef (up: UserParam) = 
+        Param.tryGetCvUnitTermRef up
+
+    /// Maps the value of the UserParam by applying the given function.
+    static member mapValue (f : ParamValue -> ParamValue) (up: UserParam) = 
+        Param.mapValue f up :?> CvParam
+
+    /// Tries mapping the value of the UserParam by applying the given function. Returns Some if it succeeds, else returns None.
+    static member tryMapValue (f : ParamValue -> ParamValue option) (up: UserParam) = 
+        Param.tryMapValue f up 
+        |> Option.map (fun v -> v :?> UserParam)
+
+    /// Tries to add the given name to the UserParam. Returns Some if possible, else returns None.
+    static member tryAddName (name : string) (up: UserParam) = 
+        Param.tryAddName name up
+        |> Option.map (fun v -> v :?> UserParam)
+
+    /// Tries to add the given accession to the UserParam. Returns Some if possible, else returns None.
+    static member tryAddAccession (acc : string) (up: UserParam) = 
+        Param.tryAddAccession acc up
+        |> Option.map (fun v -> v :?> UserParam)
+
+    /// Tries to add the given reference to the UserParam. Returns Some if possible, else returns None.
+    static member tryAddReference (ref : string) (up: UserParam) = 
+        Param.tryAddReference ref up
+        |> Option.map (fun v -> v :?> UserParam)
+
+    /// Tries to add the given CvUnit to the UserParam. Returns Some if possible, else returns None.
+    static member tryAddUnit (unit : CvUnit) (up: UserParam) = 
+        Param.tryAddUnit unit up
+        |> Option.map (fun v -> v :?> UserParam)
+
+    /// Returns the ID of the UserParam.
+    static member getCvAccession (up: UserParam) = 
+        Param.getCvAccession up
+
+    /// Returns the name of the UserParam.
+    static member getCvName (up: UserParam) = 
+        Param.getCvName up
+
+    /// Returns the reference of the UserParam.
+    static member getCvRef (up: UserParam) = 
+        Param.getCvRef up
+
+    /// Returns the full CvTerm of the UserParam.
+    static member getTerm (up: UserParam) = 
+        Param.getTerm up
+
+    /// Returns true if the given term matches the term of the UserParam.
+    static member equalsTerm (term : CvTerm) (up: UserParam) = 
+        Param.equalsTerm term up
+
+    /// Returns true if the terms of the given UserParams match.
+    static member equals (up1: UserParam) (up2: UserParam) = 
+        Param.equals up1 up2
+
+    /// Returns true if the names of the given UserParams match.
+    static member equalsName (up1: UserParam) (up2: UserParam) = 
+        Param.equalsName up1 up2
+
+
+    //---------------------- UserParam specific implementations ----------------------//
+
+    static member toCvParam (up: UserParam) = CvParam(CvTerm.create(up.Accession, up.Name, up.RefUri), up.Value, up.Attributes)
+
+    override this.ToString() = 
+        $"Name: {this.Name}\n\tValue: {this.Value}\n\tQualifiers: {this.Keys |> Seq.toList}"
+
+    member this.DisplayText = 
+        this.ToString()
+
+
+[<AutoOpen>]
+module UserParamExtensions = 
+
+    type CvParam with
+        /// Returns the UserParam of the given CvParam.
+        static member toUserParam (cvp: CvParam) =
+            UserParam(cvp.Name, cvp.Value, cvp.Attributes)
+
+    type ParamBase with
+        /// Returns Some UserParam if the given IParamBase can be downcast, else returns None.
+        static member tryUserParam (cv : IParamBase) =
+            match cv with
+            | :? UserParam as param -> Some param
+            | _ -> None
+
+    type Param with
+        /// Returns Some UserParam if the given IParam can be downcast, else returns None.
+        static member tryUserParam (cv : IParam) =
+            match cv with
+            | :? UserParam as param -> Some param
+            | _ -> None
+
+        /// Returns the CvParam of the given IParam.
+        static member toCvParam (cv : IParam) =
+            match cv with
+            | :? UserParam as up -> up |> UserParam.toCvParam
+            | :? CvParam as cvp -> cvp
+            | _ -> failwith "no conversion to CvParam available for this type"
+            
+        /// Returns the UserParam of the given IParam.
+        static member toUserParam (cv : IParam) =
+            match cv with
+            | :? UserParam as up -> up 
+            | :? CvParam as cvp -> cvp |> CvParam.toUserParam
+            | _ -> failwith "no conversion to CvParam available for this type"
+
+    type CvBase with
+        /// Returns Some UserParam if the given ICvBase can be downcast, else returns None.
+        static member tryUserParam (cv : ICvBase) =
+            match cv with
+            | :? UserParam as param -> Some param
+            | _ -> None
