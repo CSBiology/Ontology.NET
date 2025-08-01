0b5c987c4a75a7aa003915d15b742a86f124fcca
Oliver Maus <oliver.maus@eclipso.de>
2025-08-01 18:16:45 +0200

    (WIP) Add functionality for parsing to OboOntology

--- src/Ontology.NET/Ontology.fs
+++ src/Ontology.NET/Ontology.fs
@@ -147,6 +147,23 @@ type Ontology() =
 
         onto
 
+    static member toOboOntology headerData (onto : Ontology) =
+
+        let terms = 
+            onto.GetTerms() 
+            |> Seq.map snd
+            |> Seq.map (
+                fun cvt -> 
+                    let relations = onto.GetTargetTermRelations(cvt.Accession)
+                    OboTerm.Create(
+                        cvt.Accession, 
+                        Name = cvt.Name
+                        Relationships = 
+                    )
+            )
+
+        OboOntology.Create
+
 
     // basic functionality:
 
